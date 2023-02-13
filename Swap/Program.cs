0462e3db7ed7f3a1045e14ed8ec9231e62eeb853
Riya Fathima <riya.fathima@experionglobal.local>
2023-02-13 14:58:54 +0530

    commit successfully

---
+++ Swap/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number(a): ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("a ="+a);
+            
+            Console.WriteLine("Enter second number(b):");
+            
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("b =" + b);
+            Console.WriteLine("After Swap: ");
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("a ="+ a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+}
